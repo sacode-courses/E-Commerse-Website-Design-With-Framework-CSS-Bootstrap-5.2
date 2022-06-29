7899bfea61c14fd72b2ee35761c92266ce8f2691
Samuel Bosawer <zam.bosawer@gmail.com>
2022-06-30 00:05:50 +0900

    firt commit

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:4,
+    loop:true,
+    autoWidth:true,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true,
+    responsive:{
+  0:{
+      items:1,
+      nav:true
+  },
+  600:{
+      items:2,
+      nav:false
+  },
+  1000:{
+      items:3,
+      nav:true,
+  }
+}
+});
